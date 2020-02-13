1e127a6e7a1d11951db2e2730b30ae6cfa8219e7
Juan <ja.arizag@uniandes.edu.co>
2020-02-12 22:31:40 -0500

    Terminado, Pila, Lista y Test Terminados

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -39,89 +39,102 @@ public class Controller {
 
 		while( !fin ){
 			view.printMenu();
+			try
+			{
+				int option = Integer.parseInt(lector.next());				
+				switch(option){
+					case 1:
+						view.printMessage("--------- \nCargando lista de comparendos en la pila y en la cola ");
+					    try
+					    {
+					    	modelo.cargarComparendos(RUTA);
+					    	view.printMessage("Pila y Cola de comparendos creada");
+					    	view.printMessage("PRIMERO: \n"+modelo.darPrimerComparendo().toString());
+					    	view.printMessage("ÚLTIMO: \n"+modelo.darUltimoComparendo().toString());
+					    }
+					    catch(FileNotFoundException e)
+					    {
+					    	view.printMessage("No se pudo crear la lista porque no existe el archivo de comparendos");
+					    }
+					    view.printMessage("\n---------\n" + "Numero actual de comparendos en la pila " + modelo.darTamanoPila()+"\n");	
+					    view.printMessage("Numero actual de comparendos en la cola " + modelo.darTamanoCola() + "\n---------");	
+					    break;
 
-			int option = lector.nextInt();				
-			switch(option){
-				case 1:
-					view.printMessage("--------- \nCargando lista de comparendos en la pila y en la cola ");
-				    try
-				    {
-				    	modelo.cargarComparendos(RUTA);
-				    	view.printMessage("Pila y Cola de comparendos creada");
-				    	view.printMessage("PRIMERO: \n"+modelo.darPrimerComparendo().toString());
-				    	view.printMessage("ÚLTIMO: \n"+modelo.darUltimoComparendo().toString());
-				    }
-				    catch(FileNotFoundException e)
-				    {
-				    	view.printMessage("No se pudo crear la lista porque no existe el archivo de comparendos");
-				    }
-				    view.printMessage("\n---------\n" + "Numero actual de comparendos en la pila " + modelo.darTamanoPila()+"\n");	
-				    view.printMessage("Numero actual de comparendos en la cola " + modelo.darTamanoCola() + "\n---------");	
-				    break;
-
-				case 2:
-					view.printMessage("--------- \n ");
-					cola = modelo.darColaInfracciones();
-					
-					if(cola==null)
-					{
-						view.printMessage("La cola está vacía");
-					}
-					else
-					{
-						view.printMessage("Hay un total de "+cola.size()+" comparendos seguidos con la infracción "+ cola.peek().darInfraccion()+"\n");	
-						view.printMessage(cola.darLista()+"\n");
-					}
-					
-					view.printMessage("Numero actual de comparendos en la cola " + modelo.darTamanoCola() + "\n---------");						
-					break;
-				
-				case 3:
-					view.printMessage("--------- \n ");
-					view.printMessage("Ingrese el comparendo que desee buscar: ");
-					String inf = lector.next().trim();
-					view.printMessage("Ingrese la cantidad de comparendos que quiere: ");
-					int valor = lector.nextInt(); 
-					
-					cola = modelo.darColaNUltimos(valor, inf);
-					
-					if(cola==null)
-					{
-						view.printMessage("La pila está vacía");
-					}
-					else
-					{
-						if(cola.size()==valor)
+					case 2:
+						view.printMessage("--------- \n ");
+						cola = modelo.darColaInfracciones();
+						
+						if(cola==null)
 						{
-							view.printMessage("Se encontraron los  "+cola.size()+" comparendos con la infracción "+ cola.peek().darInfraccion()+"\n");	
-						}
-						else if(cola.size()==0)
-						{
-							view.printMessage("No se encontraron comparendos con la infracción "+inf);
+							view.printMessage("La cola está vacía");
 						}
 						else
 						{
-							view.printMessage("Solamente se encontraron "+cola.size()+" comparendos con la infraccion "+inf);
+							view.printMessage("Hay un total de "+cola.size()+" comparendos seguidos con la infracción "+ cola.peek().darInfraccion()+"\n");	
+							view.printMessage(cola.darLista()+"\n");
 						}
-						view.printMessage(cola.darLista()+"\n");
-					}
-					view.printMessage("Numero actual de comparendos en la pila " + modelo.darTamanoPila() + "\n---------");
-					break;
-				
-				
-				
+						
+						view.printMessage("Numero actual de comparendos en la cola " + modelo.darTamanoCola() + "\n---------");						
+						break;
 					
-				case 4: 
-					view.printMessage("--------- \n Hasta pronto !! \n---------"); 
-					lector.close();
-					fin = true;
-					break;
+					case 3:
+						view.printMessage("--------- \n ");
+						view.printMessage("Ingrese el comparendo que desee buscar: ");
+						String inf = lector.next().trim();
+						view.printMessage("Ingrese la cantidad de comparendos que quiere: ");
+						
+						try
+						{
+							int valor = Integer.parseInt(lector.next());
+							cola = modelo.darColaNUltimos(valor, inf);
+							
+							if(cola==null)
+							{
+								view.printMessage("La pila está vacía");
+							}
+							else
+							{
+								if(cola.size()==valor)
+								{
+									view.printMessage("Se encontraron los  "+cola.size()+" comparendos con la infracción "+ cola.peek().darInfraccion()+"\n");	
+								}
+								else if(cola.size()==0)
+								{
+									view.printMessage("No se encontraron comparendos con la infracción "+inf);
+								}
+								else
+								{
+									view.printMessage("Solamente se encontraron "+cola.size()+" comparendos con la infraccion "+inf);
+								}
+								view.printMessage(cola.darLista()+"\n");
+							}
+						}
+						catch(NumberFormatException e)
+						{
+							view.printMessage("Ingrese un número válido de comparendos");
+						}
+						view.printMessage("Numero actual de comparendos en la pila " + modelo.darTamanoPila() + "\n---------");
+						break;
+					
+					
+					
+						
+					case 4: 
+						view.printMessage("--------- \n Hasta pronto !! \n---------"); 
+						lector.close();
+						fin = true;
+						break;
 
-				default: 
-					view.printMessage("--------- \n Opcion Invalida !! \n---------");
-					break;
+					default: 
+						view.printMessage("--------- \n Opcion Invalida !! \n---------");
+						break;
+				}
 			}
+			catch(NumberFormatException e)
+			{
+				view.printMessage("Por favor ingrese un número");
+			}
+			
 		}
-		
 	}	
 }
